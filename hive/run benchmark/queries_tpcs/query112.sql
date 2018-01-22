fe6463f970e9b70f190c0bcec240425d98928283
Daniel Reider <daniel.reider@open-ls.de>
2018-01-22 16:31:47 +0100

    added and modified bigtable queries

--- hive/run benchmark/queries_tpcs/query112.sql
+++ hive/run benchmark/queries_tpcs/query112.sql
@@ -1,14 +1,11 @@
-
-select  count(*) as c
+select count(*) as c
 from store_sales
-    ,household_demographics
-    ,time_dim, store
-where ss_sold_time_sk = time_dim.t_time_sk
-    and ss_hdemo_sk = household_demographics.hd_demo_sk
-    and ss_store_sk = s_store_sk
-    and time_dim.t_hour = 8
-    and time_dim.t_minute >= 30
-    and household_demographics.hd_dep_count = 5
-    and store.s_store_name = 'ese'
+join household_demographics on store_sales.ss_hdemo_sk = household_demographics.hd_demo_sk
+join time_dim on store_sales.ss_sold_time_sk = time_dim.t_time_sk
+join store on store_sales.ss_store_sk = store.s_store_sk
+where time_dim.t_hour = 8
+  and time_dim.t_minute >= 30
+  and household_demographics.hd_dep_count = 5
+  and store.s_store_name = 'ese'
 order by c
 limit 100;
