4578cd86acebc34d3d2bd149429e60a83a5fefca
marco135 <mkuepers@gmx.de>
2018-01-23 22:56:47 +0100

    Ausgangsqueries hinzugefügt

---
+++ basis_queries/query_6.sql
@@ -0,0 +1,22 @@
+
+select  a.ca_state state, count(*) cnt
+ from customer_address a
+     ,customer c
+     ,store_sales s
+     ,date_dim d
+     ,item i
+ where       a.ca_address_sk = c.c_current_addr_sk
+ 	and c.c_customer_sk = s.ss_customer_sk
+ 	and s.ss_sold_date_sk = d.d_date_sk
+ 	and s.ss_item_sk = i.i_item_sk
+ 	and d.d_month_seq = 
+ 	     (select distinct (d_month_seq)
+ 	      from date_dim
+               where d_year = 2000
+ 	        and d_moy = 2 )
+ group by a.ca_state
+ /* having count(*) >= 10 wird im Java Result berücksichtigt */
+ order by cnt 
+ limit 100;
+
+
