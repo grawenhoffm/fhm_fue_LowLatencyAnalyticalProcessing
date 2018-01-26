cefb5978eb0ae19ec5adce0bf13cdacbf0eb6575
marco135 <mkuepers@gmx.de>
2018-01-26 23:02:20 +0100

    Move files

--- elasticsearch_mk/ElasticSearch/ElasticSearchSparkImporter/src/main/resources/inventory_fullouterjoin.sql
+++
@@ -1,5 +0,0 @@
-select * from inventory 
-  full outer join date_dim on inv_date_sk = d_date_sk
-  full outer join item on inv_item_sk = i_item_sk
-  full outer join warehouse on inv_warehouse_sk = w_warehouse_sk
-
